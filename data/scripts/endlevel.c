d4746a8178ec2e47bb7bc07bfefc3202116a35de
Damon V. Caskey <dc@caskeys.com>
2018-03-29 12:03:47 -0400

    Remove unused level initial and close scripts.

--- data/scripts/endlevel.c
+++
@@ -1,8 +0,0 @@
-#include "data/scripts/vars/constants.h"	//http://www.caskeys.com/dc/?p=1314#constants
-#import "data/scripts/trailerd.c"
-
-//cache most often used things here
-void main()
-{
-	tclear();
-}
--- data/scripts/level.c
+++
@@ -1,8 +0,0 @@
-#include "data/scripts/vars/constants.h"	//http://www.caskeys.com/dc/?p=1314#constants
-#import "data/scripts/com/soun0004.h"
-
-//cache most often used things here
-void main()
-{
-    soun0004();														//Sound preloader.
-}

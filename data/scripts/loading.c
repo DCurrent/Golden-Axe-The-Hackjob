2d182a3eec58ebf843469d3705568d56428eaa44
Caskey, Damon V <dc@caskeys.com>
2021-03-27 14:47:49 -0400

    Ax Battler uses current sound system for battle cries. Loading has updated documentation and sound file setup moved to dedicated function.

--- data/scripts/loading.c
+++ data/scripts/loading.c
@@ -1,25 +1,53 @@
 #include "data/scripts/dc_fidelity/main.c"
 
-void oncreate()
+/*
+* 
+* Initialize sound references used by dc_fidelity
+* sound player library. Each sound reference
+* consists of a Category, Type, and Path.
+*
+* Category - Generally the item sound belongs to.
+* Most commonly this is a model name, which is
+* later furnished automatically when a sound
+* playback is requested. It is possible to define
+* and use custom categories as well. Finally,
+* there is a global category that is used when a
+* sound request is made but the supplied category
+* does not have a sound reference of the requested
+* type (see below) defined for it here.
+*
+* Type - Type of sound to play. Examples would be
+* attack shouts, hello greetings, KO screams and
+* so on. Each category is allowed unlimited definitions
+* of any sound type. When playback is requested for
+* the sound type, the sound player randomly selects
+* one reference to play. it is also possible to request
+* playback of a specific reference by number.
+* Reference numbers for each type are 0 indexed in
+* order of definition. The sound player has a wide
+* selection of category constants ready for use, or
+* you may write in your own.
+*
+* Path - Path to the sound file. You may use the same
+* sound path for multiple references with no memory
+* penalty - the actual sound file is only loaded once.
+*/
+void load_sounds()
 {
-	//log("\n");
-	//log("loaded.c");
-	//log("\t");
-	//log("- oncreate().");
-	//log("\n");
+	/***** Global *****/
 
-	// Global sounds.
+	/* Global Sounds. */
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_LIGHT, "data/sounds/custom/golden_axe_whiff_light_0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_MEDIUM, "data/sounds/custom/rb-whiff-med-0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_MEDIUM_BLADE, "data/sounds/custom/samurai_shodown_whiff_light_0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_MEDIUM_BLADE, "data/sounds/custom/samurai_shodown_whiff_light_1.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_HEAVY, "data/sounds/custom/rb-whiff-heavy-0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_HEAVY, "data/sounds/custom/rb-whiff-heavy-1.wav");
-	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_HEAVY_BLADE, "data/sounds/custom/samurai_shodown_whiff_heavy_0.wav");	
+	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_HEAVY_BLADE, "data/sounds/custom/samurai_shodown_whiff_heavy_0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, "golden_axe_special", "data/sounds/custom/golden_axe_special.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, "golden_axe_heavy", "data/sounds/custom/golden_axe_whiff_heavy.wav");
 
-	// Global voices.
+	/* Global voices. */
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_VOICE_KO, "data/sounds/custom/avp_soldier_ko_0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_VOICE_KO, "data/sounds/custom/avp_soldier_ko_1.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_VOICE_KO, "data/sounds/custom/fatal_fury_rebout_ko.wav");
@@ -48,15 +76,14 @@ void oncreate()
 
 	/* Slam, heavy. */
 	dc_fidelity_setup("slam_heavy", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/unknown_slam.wav");
-	
+
 	/* Slam, light. */
 	dc_fidelity_setup("slam_light", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/world_heroes_slam_0.wav");
 
-	// Block flash.
+	/* Block flash. */
 	dc_fidelity_setup("block", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/cvs_block.wav");
 
 	/*  Hit flash */
-	// dc_fidelity_setup("Flash", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/double_dragon_2_hit_0.wav");
 	dc_fidelity_setup("flash_fatal_fury", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/fatal_fury_1_hit.wav");
 	dc_fidelity_setup("flash_hit_knockdown", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/golden_axe_hit_knockdown_0.wav");
 	dc_fidelity_setup("flash_indirect", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/sor1_hit_0.wav");
@@ -67,6 +94,25 @@ void oncreate()
 
 	dc_fidelity_setup("flash_impact_cut", DC_FIDELITY_TYPE_SOUND_SPAWN, "data/sounds/custom/golden_axe_hit_ram_0.wav");
 
+	/* Ax Battler */
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_LIGHT, "data/chars/ax/sounds/voice_attack_0.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_LIGHT, "data/chars/ax/sounds/voice_attack_1.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_LIGHT, "data/chars/ax/sounds/voice_attack_2.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_LIGHT, "data/chars/ax/sounds/voice_attack_3.wav");
+
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_MEDIUM, "data/chars/ax/sounds/voice_attack_1.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_MEDIUM, "data/chars/ax/sounds/voice_attack_2.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_MEDIUM, "data/chars/ax/sounds/voice_attack_3.wav");
+
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/ax/sounds/voice_attack_1.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/ax/sounds/voice_attack_2.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/ax/sounds/voice_attack_3.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/ax/sounds/voice_ga2_stern_special_0.wav");
+
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/ax/sounds/voice_battle_cry_destroy.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/ax/sounds/voice_battle_cry_die.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/ax/sounds/voice_battle_cry_fall.wav");
+	dc_fidelity_setup("ax", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/ax/sounds/voice_battle_cry_steel.wav");
 
 	/* Longmoan */
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_LIGHT, "data/chars/longmoan/sounds/atk_shout_0.wav");
@@ -78,13 +124,13 @@ void oncreate()
 
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/longmoan/sounds/atk_shout_1.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/longmoan/sounds/atk_shout_2.wav");
-	
+
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/longmoan/sounds/battle_taunt_0.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/longmoan/sounds/battle_taunt_1.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/longmoan/sounds/battle_taunt_2.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_BATTLE_TAUNT, "data/chars/longmoan/sounds/battle_taunt_3.wav");
-	
-	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_PAIN, "data/chars/longmoan/sounds/hurt_0.wav"); 
+
+	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_PAIN, "data/chars/longmoan/sounds/hurt_0.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_PAIN, "data/chars/longmoan/sounds/hurt_1.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_PAIN, "data/chars/longmoan/sounds/hurt_2.wav");
 
@@ -95,7 +141,7 @@ void oncreate()
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_NEAR_KO, "data/chars/longmoan/sounds/low_hp_0.wav");
 	dc_fidelity_setup("longmoan", DC_FIDELITY_TYPE_VOICE_NEAR_KO, "data/chars/longmoan/sounds/low_hp_1.wav");
 
-	//Chickenlegs
+	/* Chickenlegs */
 	dc_fidelity_setup("Chickenlegs", DC_FIDELITY_TYPE_VOICE_SPAWN_NEW, "data/chars/chicken/sounds/atk_shout_0.wav");
 	dc_fidelity_setup("Chickenlegs", DC_FIDELITY_TYPE_VOICE_SPAWN_NEW, "data/chars/chicken/sounds/atk_shout_1.wav");
 	dc_fidelity_setup("Chickenlegs", DC_FIDELITY_TYPE_VOICE_SPAWN_NEW, "data/chars/chicken/sounds/atk_shout_2.wav");
@@ -112,7 +158,7 @@ void oncreate()
 	dc_fidelity_setup("Chickenlegs", DC_FIDELITY_TYPE_VOICE_ATTACK_SHOUT_HEAVY, "data/chars/chicken/sounds/atk_shout_2.wav");
 
 	dc_fidelity_setup("Chickenlegs", "piledriver", "data/chars/chicken/sounds/loud_cry_0.wav");
-	
+
 	/* Red_Dragon */
 	dc_fidelity_setup("Red_Dragon", DC_FIDELITY_TYPE_VOICE_SPAWN_NEW, "data/chars/dragon/sounds/loud_cry_0.wav");
 
@@ -153,6 +199,17 @@ void oncreate()
 	dc_fidelity_setup("Thief", DC_FIDELITY_TYPE_VOICE_KO, "data/chars/thief/sounds/pain_0.wav");
 
 	dc_fidelity_setup("escthief", DC_FIDELITY_TYPE_VOICE_KO, "data/chars/thief/sounds/pain_0.wav");
+}
+
+void oncreate()
+{
+	//log("\n");
+	//log("loaded.c");
+	//log("\t");
+	//log("- oncreate().");
+	//log("\n");
+
+	load_sounds();
 
 }
 

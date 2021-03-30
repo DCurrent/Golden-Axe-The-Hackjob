56848ddcc827526114956e10209b9c137b3df109
Caskey, Damon V <dc@caskeys.com>
2021-03-30 13:41:58 -0400

    dc_common_block - Import legacy Z-block.

---
+++ data/scripts/dc_common_onblock/event_onblockz_enemy.c
@@ -0,0 +1,9 @@
+#include "data/scripts/dc_common_onblock/main.c"
+
+/*
+* Run by most player types as their onblockz (z boundary) script.
+*/
+
+void main()
+{
+}
--- data/scripts/z_blockz.c
+++
@@ -1,8 +0,0 @@
-//Generic Z boundry blocked script.
-
-//#import "data/scripts/com/bloz0001.h"
-
-void main()
-{
-//    bloz0001();       
-}

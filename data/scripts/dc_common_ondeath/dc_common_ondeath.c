ce57c2a94f5aa33d7b30e55a23517ae44f5bace9
Caskey, Damon V <dc@caskeys.com>
2021-03-30 14:39:48 -0400

    Create dc_common_ondeath library and import legacy z_death.

---
+++ data/scripts/dc_common_ondeath/dc_common_ondeath.c
@@ -0,0 +1,18 @@
+#include "data/scripts/dc_common_ondeath/config.h"
+
+/*
+* Caskey, Damon V
+* 2021-03-30
+* ~ Refactored from deat0001 (2009).
+*
+* Common ondeath event function used by
+* most models.
+*/
+void dc_common_ondeath()
+{
+	/* 
+	* 2021-03-30 - Golden Axe doesn't need 
+	* death function for now. Removing all 
+	* cruft from previous import.
+	*/
+}
---
+++ data/scripts/dc_common_ondeath/event_ondeath_enemy.c
@@ -0,0 +1,10 @@
+#include "data/scripts/dc_common_ondeath/main.c"
+
+/*
+* Run by most enemy types as their didhitscript. 
+*/
+
+void main()
+{
+	dc_common_ondeath();
+}

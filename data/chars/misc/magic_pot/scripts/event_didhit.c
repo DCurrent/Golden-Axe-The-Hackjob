39aa251259df4cf36c053fd010724c39a8f401c1
Caskey, Damon V <dc@caskeys.com>
2021-04-04 23:08:56 -0400

    Magic pot - Play arcade collect sound on pickup. Bounce on landing. Use .png assets. Reorganized into magic_pot folder.

---
+++ data/chars/misc/magic_pot/scripts/event_didhit.c
@@ -0,0 +1,6 @@
+#include "data/scripts/dc_fidelity/main.c"
+
+void main()
+{
+	dc_fidelity_quick_play("golden_axe_item_get");
+}
--- data/scripts/loading.c
+++ data/scripts/loading.c
@@ -46,6 +46,7 @@ void load_sounds()
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_SOUND_ATTACK_HEAVY_BLADE, "data/sounds/custom/samurai_shodown_whiff_heavy_0.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, "golden_axe_special", "data/sounds/custom/golden_axe_special.wav");
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, "golden_axe_heavy", "data/sounds/custom/golden_axe_whiff_heavy.wav");
+	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, "golden_axe_item_get", "data/sounds/custom/golden_axe_item_get_0.wav");
 
 	/* Global voices. */
 	dc_fidelity_setup(DC_FIDELITY_CATEGORY_GLOBAL, DC_FIDELITY_TYPE_VOICE_KO, "data/sounds/custom/avp_soldier_ko_0.wav");

319334e885d75779cf868b05700d38622718eda7
Caskey, Damon V <dc@caskeys.com>
2021-04-07 13:14:52 -0400

    Longmoan - Add and test alpha masked motion trail to opening melee attack. To-do: Works as intended, implement for all animations.

--- data/chars/longmoan/scripts/event_animation.c
+++ data/chars/longmoan/scripts/event_animation.c
@@ -17,4 +17,5 @@
 #import "data/scripts/com/dc_in_screen_setup.c"	//Check_increen
 
 #include "data/scripts/dc_elmers/main.c"
+#include "data/scripts/dc_gauntlet/main.c"
 #include "data/scripts/dc_fidelity/main.c"

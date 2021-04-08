e1810d4dbab7538c22c6c15b13dd9f6083e3bbda
Caskey, Damon V <dc@caskeys.com>
2021-04-07 23:39:24 -0400

    Longmoan - event_animation - remove unused proj0003.

--- data/chars/longmoan/scripts/event_animation.c
+++ data/chars/longmoan/scripts/event_animation.c
@@ -10,7 +10,6 @@
 #import "data/scripts/com/bind0014.h"
 #import "data/scripts/com/dama0004.h"
 #import "data/scripts/com/effe0001.h"
-#import "data/scripts/com/proj0003.h"
 #import "data/scripts/com/soun0005.h"
 
 #import "data/scripts/com/wake0001.h"

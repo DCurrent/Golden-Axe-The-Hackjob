5847b845f0cbf7dd85459cc79e1eb545dc229f23
Caskey, Damon V <dc@caskeys.com>
2021-04-11 10:16:46 -0400

    New global take damage dc_common_takedamage. Not yet implemented.

---
+++ data/scripts/dc_common_takedamage/dc_common_takedamage.c
@@ -0,0 +1,28 @@
+#include "data/scripts/dc_common_takedamage/config.h"
+
+/*
+* Caskey, Damon V
+* 2021-04-11
+*
+* Common takedamage event function used by
+* most models.
+*/
+void dc_common_takedamage()
+{
+    /*
+
+    void    acting_entity = getlocalvar("self"); 
+    int     attack_type = getlocalvar("attacktype");
+    
+    if (attack_type == openborconstant("ATK_BURN"))
+    {
+    }
+    else if (attack_type == openborconstant("ATK_SHOCK"))
+    {     
+    }
+    else
+    {        
+    }
+      
+    */
+}

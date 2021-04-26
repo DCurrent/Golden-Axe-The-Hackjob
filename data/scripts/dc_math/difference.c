e6ccf5278416323b40a805fe2271407b0ec37d57
Caskey, Damon V <dc@caskeys.com>
2021-04-26 00:57:00 -0400

    dc_math - Add int difference function.

--- data/scripts/dc_math/difference.c
+++ data/scripts/dc_math/difference.c
@@ -1,10 +1,28 @@
 #include "data/scripts/dc_math/config.h"
 
-// Caskey, Damon V.
-// 2018-11-24
-//
-// Find difference between two floating point values.
+/* 
+* Caskey, Damon V.
+* 2018-11-24
+*
+* Find difference between two floating point values.
+*/
 float dc_math_difference_float(float a, float b)
+{
+	if (a < b)
+	{
+		return b - a;
+	}
+	return a - b;
+}
+
+
+/*
+* Caskey, Damon V.
+* 2018-11-24
+*
+* Find difference between two integer values.
+*/
+int dc_math_difference_int(int a, int b)
 {
 	if (a < b)
 	{

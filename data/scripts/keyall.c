0ee58bc2faea317a4ed5c7dc7cb78344df14afc5
Caskey, Damon V <dc@caskeys.com>
2021-05-11 00:06:21 -0400

    Remove debu0001.h

--- data/scripts/keyall.c
+++ data/scripts/keyall.c
@@ -2,7 +2,6 @@
 #include "data/scripts/vars/constants.h"	//http://www.caskeys.com/dc/?p=1314#constants
 #import "data/scripts/com/ani0009.h"		//http://www.caskeys.com/dc/?p=1314#ani0009
 #import "data/scripts/com/ani0020.h"		//http://www.caskeys.com/dc/?p=1314#ani0020
-#import "data/scripts/com/debu0001.h"		//http://www.caskeys.com/dc/?p=1314#debu0001
 #import "data/scripts/com/soun0005.h"		//http://www.caskeys.com/dc/?p=1314#soun0005
 
 void main(){

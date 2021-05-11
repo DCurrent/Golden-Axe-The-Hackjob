d055920c08831247e95dd1a8f3a5da9564d00e22
Caskey, Damon V <dc@caskeys.com>
2021-05-11 12:08:27 -0400

    Remove z_block.c and z_block.h support functions.

--- data/scripts/z_block.c
+++
@@ -1,7 +0,0 @@
-#import "data/scripts/com/z_block.h" //Global block function.
-
-void main()
-{	
-	void vEnt = getlocalvar("self");
-	z_block(vEnt);                                                                 //Run global block function.
-}

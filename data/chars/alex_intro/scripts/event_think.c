14b537ccd880ede89baa932109188f4ecf28cfb3
Caskey, Damon V <dc@caskeys.com>
2021-06-07 00:07:35 -0400

    Alex Intro - Now walks to destination spot and dies.

---
+++ data/chars/alex_intro/scripts/event_think.c
@@ -0,0 +1,23 @@
+
+void main()
+{
+	void acting_entity = getlocalvar("self");
+
+	float pos_x = get_entity_property(acting_entity, "position_x");
+	float pos_z = get_entity_property(acting_entity, "position_z");
+	
+
+
+	if (pos_x - 170.0 < 1.0 && pos_z - 150 < 1.0)
+	{		
+		int health = 1000; // = get_entity_property(acting_entity, "hp");
+		damageentity(acting_entity, acting_entity, health, 0);
+	}
+	else
+	{
+		log("\n (no) pos: " + pos_x + ", " + pos_z);
+		set_entity_property(acting_entity, "destination_x", 170);
+		set_entity_property(acting_entity, "destination_z", 150);
+	}
+	
+}

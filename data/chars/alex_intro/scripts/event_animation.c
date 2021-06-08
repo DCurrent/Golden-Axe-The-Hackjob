30172314bc4b3993ff6439700665a169036fbd2b
Caskey, Damon V <dc@caskeys.com>
2021-06-08 13:46:33 -0400

    Alex Intro - Text box dialog follows step 1 and 2. Todo:
    - Run all steps.
    - Adapt to height of model.
    - Switch between main character and guest dialogs.

--- data/chars/alex_intro/scripts/event_animation.c
+++ data/chars/alex_intro/scripts/event_animation.c
@@ -1,29 +1,35 @@
 #include "data/scripts/dc_elmers/main.c"
-#include "data/scripts/dc_guantlet/main.c"
+#include "data/scripts/dc_gauntlet/main.c"
 
 
 /* spawn 3 text boxes, in a chain. */
-void checktext()
+void dc_run_dialog()
 {
     int dialog_step_0 = getlocalvar("dialog_step_0");
     int dialog_step_1 = getlocalvar("dialog_step_1");
 
     void self = getlocalvar("self");
     
+    /* 
+    * Entity that talks to Alex. This was populated 
+    * by dc_dialog_setup() function in event_think.
+    */
+    void dialog_player_entity = getentityvar(self, "dialog_player_entity");
+    
     void dialog_entity_0 = NULL();
     void dialog_entity_1 = NULL();
     void dialog_entity_2 = NULL();
-
-    void bind = NULL();
-
+       
     if (!dialog_step_0)
     {
         dialog_entity_0 = dc_gauntlet_quick_spawn("alex_intro_dialog_0");
 
-        
+        dc_dialog_bind(dialog_entity_0, dialog_player_entity);
        
         setlocalvar("dialog_step_0", 1);
         setlocalvar("alex_intro_dialog_0", dialog_entity_0);
+
+        log("\n step 0");
     }
     else if (!dialog_step_1)
     {
@@ -33,20 +39,26 @@ void checktext()
         * next dialog model.
         */
 
+        log("\n step 1");
+
         dialog_entity_0 = getlocalvar("alex_intro_dialog_0");
 
-        if (dialog_entity_0 && get_entity_property(text1, "animation_state") == openborconstant("ANIMATING_NONE"))
+        if(dialog_entity_0) 
         {
-            killentity(dialog_entity_0);
-            setlocalvar("alex_intro_dialog_0", NULL());    
-            
-            // spawn the text2 that Alex spoke
-            dialog_entity_1 = dc_gauntlet_quick_spawn("alex_intro_dialog_1");
+            if(get_entity_property(dialog_entity_0, "animation_state") == 0)
+            {
+                killentity(dialog_entity_0);
+                setlocalvar("alex_intro_dialog_0", NULL());
 
-            // move the text 53 above Alex's head
-            
-            setlocalvar("dialog_step_1", 1);
-            setlocalvar("alex_intro_dialog_1", dialog_entity_1);
+                // spawn the text2 that Alex spoke
+                dialog_entity_1 = dc_gauntlet_quick_spawn("alex_intro_dialog_1");
+
+                // move the text above Alex's head
+                dc_dialog_bind(dialog_entity_0, self);
+
+                setlocalvar("dialog_step_1", 1);
+                setlocalvar("alex_intro_dialog_1", dialog_entity_1);
+            }
         }
     }
     else if (dialog_step_1)
@@ -60,29 +72,37 @@ void checktext()
 
         dialog_entity_1 = getlocalvar("alex_intro_dialog_1");
         
-        if (dialog_entity_1 && get_entity_property(text2, "animation_state") == openborconstant("ANIMATING_NONE"))
+        if (dialog_entity_1)
         {
-            killentity(dialog_entity_1);
-            setlocalvar("alex_intro_dialog_1", NULL());
-            
-            // spawn text3: Alex...
-            dialog_entity_2 = dc_gauntlet_quick_spawn("alex_intro_dialog_2");
-                        
-            changeentityproperty(self, "animation", openborconstant("ANI_DIE10"));
-            setlocalvar("alex_intro_dialog_2", dialog_entity_2);
+            if (get_entity_property(dialog_step_1, "animation_state") == 0)
+            {
+                killentity(dialog_entity_1);
+                setlocalvar("alex_intro_dialog_1", NULL());
+
+                // spawn text3: Alex...
+                dialog_entity_2 = dc_gauntlet_quick_spawn("alex_intro_dialog_2");
+
+                changeentityproperty(self, "animation", openborconstant("ANI_DIE10"));
+                setlocalvar("alex_intro_dialog_2", dialog_entity_2);
+            }
         }
-    }// end of if
+    }
 }
 
-void dc_dialog_bind(void acting_entity, void target_entity)
+void dc_dialog_bind(void dialog_entity, void target_entity)
 {
-    bind = get_entity_property(bind, dialog_entity_0);
+    void bind = get_entity_property(dialog_entity, "bind");
 
-    set_bind_property(bind, "target", self);
+    set_bind_property(bind, "target", target_entity);
     set_bind_property(bind, "animation_match", openborconstant("BIND_ANIMATION_NONE"));
     set_bind_property(bind, "direction", openborconstant("DIRECTION_ADJUST_RIGHT"));
+
+    set_bind_property(bind, "mode_x", openborconstant("BIND_MODE_TARGET"));
+    set_bind_property(bind, "mode_y", openborconstant("BIND_MODE_TARGET"));
+    set_bind_property(bind, "mode_z", openborconstant("BIND_MODE_TARGET"));
+
     set_bind_property(bind, "offset_x", 0);
-    set_bind_property(bind, "offset_y", 53);
+    set_bind_property(bind, "offset_y", 70);
     set_bind_property(bind, "offset_z", 0);
 
     set_bind_property(bind, "sort_id", 1);
--- data/chars/alex_intro/scripts/event_think.c
+++ data/chars/alex_intro/scripts/event_think.c
@@ -12,8 +12,8 @@ void main()
 * Caskey, Damon V.
 * 2021-06-07 (replaces legacy move commands in idle animation)
 * 
-* Force entity's AI to walk to spot, then 
-* die using normal damage.
+* Force entity's AI to walk to spot. Spot is reached or
+* a player entity is near by, set up dialog.
 */
 void dc_alex_intro_walk(void acting_entity, float goal_pos_x, float goal_pos_z)
 {	
@@ -21,8 +21,9 @@ void dc_alex_intro_walk(void acting_entity, float goal_pos_x, float goal_pos_z)
 
 	/* 
 	* If we are at designated spot and not dead yet,
-	* damage ourselves enough to die. Otherwise set
-	* destination so we will walk to designated spot.
+	* run dialog setup function. It will KO us and
+	* prepare variables. Otherwise set destination 
+	* points so we will walk to designated spot.
 	*/
 
 	if (health < 1)
@@ -30,9 +31,13 @@ void dc_alex_intro_walk(void acting_entity, float goal_pos_x, float goal_pos_z)
 		return;
 	}
 
-	if (dc_check_near_player(acting_entity, 15.0) || dc_check_near_position(acting_entity, goal_pos_x, goal_pos_z, 1.0))
+	if (dc_get_first_player(acting_entity, 15.0))
 	{		
-		damageentity(acting_entity, acting_entity, health, 0);
+		dc_dialog_setup(acting_entity);
+	}
+	else if (dc_check_near_position(acting_entity, goal_pos_x, goal_pos_z, 1.0))
+	{
+		dc_dialog_setup(acting_entity);
 	}
 	else
 	{
@@ -41,6 +46,33 @@ void dc_alex_intro_walk(void acting_entity, float goal_pos_x, float goal_pos_z)
 	}
 }
 
+/*
+* Caskey, Damon V.
+* 2021-06-07
+* 
+* Kill self and set up dialog variable for first player.
+*/
+void dc_dialog_setup(void acting_entity)
+{
+	void first_player = dc_get_first_player(acting_entity, -1);
+
+	/* Kill acting entity. */
+	int health = get_entity_property(acting_entity, "hp");
+
+	damageentity(acting_entity, acting_entity, health, 0);
+
+	/* Animation script needs this to assign binding on dialog. */
+	setentityvar(acting_entity, "dialog_player_entity", first_player); // first_player);
+}
+
+/*
+* Caskey, Damon V.
+* 2021-06-07
+* 
+* Accept entity, position, and distance. Return 
+* true if entity position is within distance of 
+* position parameters.
+*/
 int dc_check_near_position(void acting_entity, float pos_x, float pos_z, float distance)
 {
 	float acting_pos_x = get_entity_property(acting_entity, "position_x");
@@ -56,7 +88,16 @@ int dc_check_near_position(void acting_entity, float pos_x, float pos_z, float d
 	return 0;
 }
 
-int dc_check_near_player(void acting_entity, float distance)
+
+/*
+* Caskey, Damon V.
+* 2021-06-08
+* 
+* Get first player entity within distance
+* parameter of acting entity. Return 0 if 
+* none found. Use -1 distance for infinite.
+*/
+void dc_get_first_player(void acting_entity, float distance)
 {
 	int i = 0;
 	int player_count = openborvariant("maxplayers");
@@ -96,15 +137,24 @@ int dc_check_near_player(void acting_entity, float distance)
 		distance_x = dc_math_difference_float(acting_pos_x, player_pos_x);
 		distance_z = dc_math_difference_float(acting_pos_z, player_pos_z);
 
+		/* 
+		* If distance is a negative number, just return
+		* player entity. 
+		*/
+		if (distance < 0)
+		{			
+			return player_entity;
+		}
+
 		if (distance_x <= distance && distance_z <= distance)
 		{
-			return 1;
+			return player_entity;
 		}
 	}
 
 	/* 
-	* Never found a player within distance, so
-	* we return false.
+	* Never found a player entity within 
+	* distance, so we return false.
 	*/
 	return 0;
 }

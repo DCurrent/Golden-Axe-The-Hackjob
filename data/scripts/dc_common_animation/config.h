8ad4d2af71d7823cf612df1cf3f2da90ca1a507a
Caskey, Damon V <dc@caskeys.com>
2021-06-16 15:16:07 -0400

    Ax Dragon - Add jump attack and run jump attack motion trails.

--- data/scripts/dc_common_animation/config.h
+++ data/scripts/dc_common_animation/config.h
@@ -9,9 +9,12 @@
 #import "data/scripts/com/dc_in_screen_setup.c"
 #import "data/scripts/dc_common_source/dc_ai_avoid_jump_attack.c"
 
-#include "data/scripts/dc_disney/main.c" 
-#include "data/scripts/dc_fidelity/main.c" 
 #include "data/scripts/dc_chain/main.c"
+#include "data/scripts/dc_disney/main.c" 
+#include "data/scripts/dc_elmers/main.c" 
+#include "data/scripts/dc_fidelity/main.c" 
+#include "data/scripts/dc_gauntlet/main.c" 
+
 
 #endif // !DC_COMMON_ANIMATION
 
